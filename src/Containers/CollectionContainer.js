674214efec4a58e6de6773c1ec09031c06803257
Matthew Kirby <matt.kirby@yahoo.com>
2020-11-29 14:13:58 -0500

    updated conditional rendering on collection page

--- src/Containers/CollectionContainer.js
+++ src/Containers/CollectionContainer.js
@@ -7,23 +7,25 @@ class CollectionContainer extends React.Component {
     return (
       <div className="collection-container">
         <h2>Collection Container</h2>
-
-        {/* Does a token exist? */}
         {localStorage.getItem("token") ?
-
           <>
-            {/* TODO If a user is logged in render their collection */}
-            <Switch>
-              <Route path="/collection/:user_plant_id" render={({match}) => {
-                // let user_plant_id = parseInt(match.params.id)
-                // let userPlant = this.state.userPlants.find(inst => inst.id === user_plant_id)
-                // return <PlantContainer userPlant={userPlant}
-                return <p>Logged In: Singe Plant Page</p>
-              }} />
-              <Route path="/collection" render={() => {
-                return <p>Logged In: Entire Collection Page</p>
-              }}/>
-            </Switch>
+            {this.props.user ?
+              <>
+                <Switch>
+                  <Route path="/collection/:user_plant_id" render={({match}) => {
+                    // let user_plant_id = parseInt(match.params.user_plant_id)
+                    // let userPlant = TODO
+                    // return <PlantContainer userPlant={userPlant}
+                    return <p>Logged In: Singe Plant Page</p>
+                  }} />
+                  <Route path="/collection" render={() => {
+                    return <p>Logged In: Entire Collection Page</p>
+                  }}/>
+                </Switch>
+              </>
+              :
+              <p>Loading...</p>
+            }
           </>
           :
           // no token in localStorage, then redirect to login
