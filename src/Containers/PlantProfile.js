355a4d4c2dd008ede88c4094cc6cfeadfc8b70d0
Matthew Kirby <matt.kirby@yahoo.com>
2020-11-30 17:25:41 -0500

    updated plantProfile render

--- src/Containers/PlantProfile.js
+++ src/Containers/PlantProfile.js
@@ -1,34 +1,56 @@
 import React from 'react'
 
-function PlantProfile({ plant }) {
-    const plantNamesArray = plant.main_species.common_names.en.map(name => {
-        return <li>{name}</li>
-    })
-      
-      const plantNativeTo = plant.main_species.distribution.native.map(locale => {
-            return <li>{locale}</li>
+class PlantProfile extends React.Component {
+    state = {
+        plant: null
+    }
+
+    componentDidMount() {
+        const token = localStorage.getItem("token")
+        fetch(`http://localhost:3000/api/v1/search/${this.props.slug}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
         })
-        
-      
-    return (
-        <>
-        {plant.img_url ? <img src={plant.img_url} alt={plant.common_name} width="200" /> : null}
-        <h3>{plant.common_name}</h3>
-        <p>Scientific Name: ({plant.scientific_name})</p>
-        <p>Genus: {plant.genus}</p>
-        <p>Year discovered: {plant.year}</p>
-        
-        <h4>Common names</h4>
-            <ul>
-            {plantNamesArray}
-            </ul>
-          
-        <h4>Native to</h4>
-            <ul>
-                {plantNativeTo}
-            </ul>
-        </>
-    )
+        .then(response => response.json())
+        .then(d => this.setState({plant: d.api_data.data}, () => console.log(this.state.plant)))
+    }
+
+    render() {
+        const plant = this.state.plant
+        return (
+            <>
+                {this.state.plant ? 
+                    <>
+                        {plant.image_url ? <img src={plant.image_url} alt={plant.common_name} width="200" /> : null}
+                        <h3>{plant.common_name}</h3>
+                        <p>Scientific Name: ({plant.scientific_name})</p>
+                        <p>Genus: {plant.main_species.genus}</p>
+                        <p>Year discovered: {plant.year}</p>
+                        
+                        <h4>Common names</h4>
+                            <ul>
+                                {this.renderPlantNames()}
+                            </ul>
+                        
+                        <h4>Native to</h4>
+                            <ul>
+                                {this.renderplantNativeTo()}
+                            </ul>
+                    </>
+                :
+                <p>Loading...</p> }
+            </>
+        )
+    }
+
+    renderPlantNames = () => {
+        return this.state.plant.main_species.common_names.en.map((name, index) => <li key={index}>{name}</li>)
+    }
+
+    renderplantNativeTo = () => {
+        return this.state.plant.main_species.distribution.native.map((locale, index) => <li key={index}>{locale}</li>)
+    } 
 }
     
 export default PlantProfile
