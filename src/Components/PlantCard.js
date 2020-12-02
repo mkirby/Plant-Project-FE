001c937fd1f01a5a064eeb704091ab1693b0d78d
Matthew Kirby <matt.kirby@yahoo.com>
2020-12-01 19:25:14 -0500

    added search result card styling

--- src/Components/PlantCard.js
+++ src/Components/PlantCard.js
@@ -13,15 +13,21 @@ class PlantCard extends React.Component {
   
   render() {
     const { plant } = this.props
-    console.log("plant card plant:", plant)
     return (
       <div className="plant-card">
-        {plant.image_url ? <img src={plant.image_url} alt={plant.common_name} height="100" /> : null}
-        <h3>{plant.common_name}</h3>
-        <p>Scientific Name: <em>{plant.scientific_name}</em></p>
-        <p>Genus: {plant.genus}</p>
-        <button onClick={() => this.props.renderModal(plant.slug)}>more info</button><br/>
-        <p>Add to collection?</p> <input name="selected" type="checkbox" checked={this.state.selected} onChange={this.handleCheckboxChange} />
+        <div className="plant-card-img">
+          {plant.image_url ? <img src={plant.image_url} alt={plant.common_name} /> : <img src={"https://static.patchplants.com/img/placeholders/patch_placeholder_grey.png"} alt={"image missing"} style={{"max-height": "100%"}}/>}
+        </div>
+        <div className="plant-card-data">
+          {plant.common_name ? <h3>{plant.common_name}</h3> : <h3>{plant.scientific_name}</h3>}
+          <p><strong>Scientific Name:</strong> <em>{plant.scientific_name}</em></p>
+          <p><strong>Genus:</strong> {plant.genus}</p>
+          <p><strong>Family:</strong> {plant.family}</p>
+          <button onClick={() => this.props.renderModal(plant.slug)}>More Info</button><br/>
+        </div>
+        <div className="plant-card-check">
+          <p><input name="selected" type="checkbox" checked={this.state.selected} onChange={this.handleCheckboxChange} /> Add to Collection</p> 
+        </div>
       </div>
     )
   }
