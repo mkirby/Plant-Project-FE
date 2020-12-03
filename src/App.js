1b104acaed4ac9a896ba8dcdcc2e06ffbbb0ad2d
Brian Feldman <brian.feldman42@gmail.com>
2020-12-03 16:10:33 +0000

    home page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Signup from "./Components/Signup"
 import Login from "./Components/Login"
 import Navbar from "./Components/Navbar"
+import Home from './Components/Home'
 import SearchContainer from "./Containers/SearchContainer"
 import CollectionContainer from './Containers/CollectionContainer';
 
@@ -93,9 +94,10 @@ class App extends React.Component {
           <Navbar user={this.state.user} logoutHandler={this.logoutHandler}/>
           <main>
             <Switch>
-              <Route path ="/signup" render={ () => <Signup submitHandler={this.signupHandler} /> } />
-              <Route path ="/login" render={ () => <Login submitHandler={this.loginHandler} /> } />
-              <Route path ="/search" render={ () => <SearchContainer user={this.state.user} updateUser={this.updateUser}/> } />
+              <Route path="/" render={Home} />
+              <Route path="/signup" render={ () => <Signup submitHandler={this.signupHandler} /> } />
+              <Route path="/login" render={ () => <Login submitHandler={this.loginHandler} /> } />
+              <Route path="/search" render={ () => <SearchContainer user={this.state.user} updateUser={this.updateUser}/> } />
               <Route path="/collection" render={ () => <CollectionContainer user={this.state.user} updateUser={this.updateUser}/>} />
             </Switch>
           </main>
---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return(
+        <div id="landing">
+            <h1>Welcome to Plant Collector</h1>
+            <h3>(the app where you can collect plants)</h3>
+        </div>
+    )
+}
+
+export default Home
